05c1230b353e2da94825493fc49b87c6cb95d748
Arturo <artguridi@gmail.com>
2026-06-29 22:34:02 +0200

    fixes to bash

--- advanced.sh
+++ advanced.sh
@@ -26,7 +26,7 @@ printf "987-123\n456-7890\n456-7890\n345-00001" | sort -n -t '-' -k 2 -> 987-123
 
 # GREP
 ## Pattern matching text
-# Basic: grep 'pattern' file.txt
+# Basic: grep 'pattern' file.txt/globfile (e.g *.txt)
 -i          : ignore case
 --include   : use only files matching that glob
 --exclude   : exclude files matching the glob
@@ -44,7 +44,7 @@ printf "987-123\n456-7890\n456-7890\n345-00001" | sort -n -t '-' -k 2 -> 987-123
 # -x        : match whole line only
 -A N        : print N lines After each match
 # -B N      : print N lines Before each match
-# -C N      : print N lines of Context around each match
+# -C N      : print N lines of Context around each match (both A and B basically)
 
 # 1) Case-insensitive search for "error" in a log file, show line numbers:
 grep -i -n "error" /var/log/syslog
@@ -54,8 +54,11 @@ grep -R --include='*.py' -n "TODO" .
 # Search for function definitions in C files and show filenames and line numbers:
 grep -R --include='*.c' -n "^\s*int [a-zA-Z_][a-zA-Z0-9_]*\s*(" .
 
-# 3) Use extended regex to match either "foo" or "bar":
-grep -E 'foo|bar' file.txt
+# 3) Use extended regex to match either "foo" or "bar"in all txt files:
+grep -E 'foo|bar' *.txt
+
+# 4) Remove lines from aall files in current and subdirs by using inverted search
+grep -r -w -v "Password:*" /home/user/*
 
 # 4) Extract email-like tokens from a file (only matching parts):
 grep -o -E '\b[[:alnum:]._%+-]+@[[:alnum:].-]+\.[[:alpha:]]{2,}\b' contacts.txt
@@ -84,6 +87,7 @@ grep -oE '\b[0-9]{3}\b' calls.log | sort | uniq -c | sort -nr
 
 # SED
 ## Stream editor for filtering and transforming text
+## Note: by default, if given a file, applies the transform line by line
 ## Common flags
 -n        : suppress automatic printing of pattern space
 -e script : add the script to be executed
@@ -93,20 +97,34 @@ grep -oE '\b[0-9]{3}\b' calls.log | sort | uniq -c | sort -nr
 
 ## Basic substitution form
 #   s/pattern/replacement/flags
-#   flags: g (global), p (print), number to replace nth match on line
+#   flags: g (global), p (print), N number to replace nth match on line (e.g only first match, second match), I case insensitive
+
 
 ## Examples
 # 1) Simple replace (all occurrences on each line):
 sed -e 's/foo/bar/g' file.txt
 
-# 2) In-place replacement with backup (replace "old" with "new"):
-sed -i.bak 's/old/new/g' file.txt
+# 2) In-place replacement starting from second occurence on (with backup):
+sed -i.bak 's/old/new/2g' file.txt
+
+# 3) Wrap the word "error" in brackets (case insensitive)
+sed -r 's/\berror\b/{&}/gI' file.txt
 
 # 3) Delete blank lines:
-sed '/^$/d' file.txt
+sed '/^$/d' file.txt > newfile
 
-# 4) Print only matching lines (useful with -n):
-sed -n '/ERROR/p' app.log
+# 4) Use / too by changing the sed construction
+sed s|/usr/loca/bin|/bin/g file > newfile
+sed s/\/usr\/local\/bin/\/bin file > newfile
+
+# 4) Print only matching lines (case-insenstive) (useful with -n):
+sed -n '/ERROR/pI' app.log
+
+# 5) Eliminate duplicate words
+sed -r 's/([a-z]+) \1/\1/'
+
+#7 ) Swap numbers around (e.g. credit cards numbers)
+sed -r 's/(\d{4}) (\d{4}) (\d{4}) (\d{4})/\4 \3 \2 \1/'
 
 # 5) Print line numbers with matching lines:
 sed -n '/pattern/{=;p;}' file.txt
@@ -122,10 +140,19 @@ sed '/^Section:/a\
 sed '/TODO/s/$/  # needs follow-up/' tasks.txt
 
 # 9) Replace only on a specific line range (lines 10-20):
-sed '10,20s/old/new/g' file.txt
+sed '10,20 s/old/new/g' file.txt
+
+# 10) Replace from line 20 until the end
+sed '20,$ s/old/new/g' file.txt
 
 # 10) Use sed as a quick tokenizer with piping (extract first field):
 echo "a:b:c" | sed 's/:.*$//'   # => a
+
+#print last 10 lines of file (instead of tail)
+# First argument is the filename
+lines=$(wc -l "$1" | awk '{print $1}' )
+start=$(( lines - 10))
+sed "1,$start d" "$1"
 
 ## Notes and tips
 # - Prefer -E for extended regex to avoid many backslashes.
